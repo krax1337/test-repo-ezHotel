32aff030db442af382f083b98d45d192f60a266f
Asanali Bulatov <w62890@student.wsiz.edu.pl>
2020-12-13 01:17:39 +0600

    db test

--- ezHotel/ezHotel/Program.cs
+++ ezHotel/ezHotel/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+using System.Drawing;
+using System.Diagnostics;
+
 
 namespace ezHotel
 {
@@ -14,6 +19,26 @@ namespace ezHotel
         [STAThread]
         static void Main()
         {
+            if (!File.Exists("mainDB.db"))
+                SQLiteConnection.CreateFile("mainDB.db");
+
+            try
+            {
+                var m_dbConn = new SQLiteConnection("Data Source=" + "mainDB.db" + ";Version=3;");
+                var m_sqlCmd = new SQLiteCommand();
+                m_dbConn.Open();
+
+                m_sqlCmd.Connection = m_dbConn;
+
+                m_sqlCmd.CommandText = "CREATE TABLE IF NOT EXISTS Catalog (id INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, book TEXT)";
+                m_sqlCmd.ExecuteNonQuery();
+
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
